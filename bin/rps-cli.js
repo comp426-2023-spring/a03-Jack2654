448160fa636482076b9f3374f8b999f25601fc1b
Jack2654 <jack.morgenstein@gmail.com>
2023-02-23 12:01:56 -0500

    second commit

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -1,15 +1,45 @@
 #!/usr/bin/env node
 import { rps } from '../lib/rpsls.js';
-console.log(rps());
-console.log(rps());
-console.log(rps());
-console.log(rps("rock"));
-console.log(rps("rock"));
-console.log(rps("rock"));
-console.log(rps("paper"));
-console.log(rps("paper"));
-console.log(rps("paper"));
-console.log(rps("scissors"));
-console.log(rps("scissors"));
-console.log(rps("scissors"));
+import minimist from 'minimist';
 
+const args = minimist(process.argv.slice(2));
+
+if(args.help || args.h) {
+	printHelp();
+    process.exit(0);
+}
+
+if(args.r || args.rules) {
+    printRules();
+    process.exit(0);
+
+}
+
+if(args._.length > 1) {
+    console.log("[ARGUMENT] is out of range.");
+    printHelp();
+    printRules();
+    process.exit(0);
+}
+
+function printHelp() {
+    console.log(`Usage: node-rps [SHOT]
+    Play Rock Paper Scissors (RPS)
+    
+      -h, --help      display this help message and exit
+      -r, --rules     display the rules and exit
+    
+    Examples:
+      node-rps        Return JSON with single player RPS result.
+                      e.g. {"player":"rock"}
+      node-rps rock   Return JSON with results for RPS played against a simulated opponent.
+                      e.g {"player":"rock","opponent":"scissors","result":"win"}`);
+}
+
+function printRules() {
+    console.log(`Examples:
+    node-rps        Return JSON with single player RPS result.
+                    e.g. {"player":"rock"}
+    node-rps rock   Return JSON with results for RPS played against a simulated opponent.
+                    e.g {"player":"rock","opponent":"scissors","result":"win"}`);
+}
